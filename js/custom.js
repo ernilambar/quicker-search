8b36bece6b36909133cdd3d06528721ec1f26668
Nilambar Sharma <nilambar@outlook.com>
2020-06-11 17:57:43 +0545

    Update code

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,19 @@
 ( function( $ ) {
-
 	$( document ).ready(function($){
-
 		var post_api_url = '';
-		if ( '' != Quicker_Search_Settings.post_type ) {
-			if ( 'post' == Quicker_Search_Settings.post_type  ) {
-				post_api_url = Quicker_Search_Settings.home_url + '/wp-json/wp/v2/posts';
+
+		if ( '' != quickerSearchSettings.post_type ) {
+			if ( 'post' == quickerSearchSettings.post_type  ) {
+				post_api_url = quickerSearchSettings.home_url + '/wp-json/wp/v2/posts';
 			}
-			else if ( 'page' == Quicker_Search_Settings.post_type ) {
-				post_api_url = Quicker_Search_Settings.home_url + '/wp-json/wp/v2/pages';
+			else if ( 'page' == quickerSearchSettings.post_type ) {
+				post_api_url = quickerSearchSettings.home_url + '/wp-json/wp/v2/pages';
 			}
 			else {
-				post_api_url = Quicker_Search_Settings.home_url + '/wp-json/wp/v2/' + Quicker_Search_Settings.post_type;
+				post_api_url = quickerSearchSettings.home_url + '/wp-json/wp/v2/' + quickerSearchSettings.post_type;
 			}
 		}
+
 		if ( '' != post_api_url ) {
 			$( "#post-search-input" ).autocomplete({
 				source: function( request, response ) {
@@ -34,11 +34,9 @@
 				},
 				minLength: 2,
 				select: function( event, ui ) {
-	                window.location.href = Quicker_Search_Settings.admin_url + 'post.php?post=' + ui.item.id + '&action=edit';
+	                window.location.href = quickerSearchSettings.admin_url + 'post.php?post=' + ui.item.id + '&action=edit';
 				}
 			}); // End #post-search-input.
 		} // End if not empty URL.
-
 	});
-
 } )( jQuery );
--- quicker-search.php
+++ quicker-search.php
@@ -5,9 +5,10 @@
  * Description: Quickly search posts or pages quickly in admin listing.
  * Version: 1.0.1
  * Author: Nilambar Sharma
- * Author URI: httpS://WWW.nilambar.net
+ * Author URI: https://WWW.nilambar.net
  * Requires at least: 5.2
  * Tested up to: 5.4
+ * GitHub Plugin URI: ernilambar/quicker-search
  * Text Domain: quicker-search
  *
  * @package Quicker_Search
@@ -15,13 +16,18 @@
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 define( 'QUICKER_SEARCH_BASENAME', basename( dirname( __FILE__ ) ) );
 define( 'QUICKER_SEARCH_DIR', rtrim( plugin_dir_path( __FILE__ ), '/' ) );
 define( 'QUICKER_SEARCH_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
 
+/**
+ * Load assets.
+ *
+ * @since 1.0.0
+ */
 function quicker_search_load_assets() {
 	wp_enqueue_style( 'quicker-search-smoothness', QUICKER_SEARCH_URL . '/css/smoothness/jquery-ui.css', '', '1.11.4' );
 
@@ -34,18 +40,19 @@ function quicker_search_load_assets() {
 
 	$correct_post_type = '';
 	if ( ! empty( $cur_screen->post_type ) ) {
-	    if ( isset( $wp_post_types[ $cur_screen->post_type ]->show_in_rest ) && 1 === absint( $wp_post_types[ $cur_screen->post_type ]->show_in_rest ) ) {
-	        $correct_post_type = $cur_screen->post_type;
-	    }
+		if ( isset( $wp_post_types[ $cur_screen->post_type ]->show_in_rest ) && 1 === absint( $wp_post_types[ $cur_screen->post_type ]->show_in_rest ) ) {
+			$correct_post_type = $cur_screen->post_type;
+		}
 	}
 
 	$custom_args = array(
-	    'home_url'  => home_url(),
-	    'admin_url' => admin_url(),
-	    'post_type' => $correct_post_type,
-	    'tax_type'  => $cur_screen->taxonomy,
+		'home_url'  => home_url(),
+		'admin_url' => admin_url(),
+		'post_type' => $correct_post_type,
+		'tax_type'  => $cur_screen->taxonomy,
 	);
-	wp_localize_script( 'quicker-search-custom', 'Quicker_Search_Settings', $custom_args );
+
+	wp_localize_script( 'quicker-search-custom', 'quickerSearchSettings', $custom_args );
 	wp_enqueue_script( 'quicker-search-custom' );
 }
 
